dfaa990a15471d4b94db2ca960b855ec2974ab52
cookiecooker <ningjm.here@gmail.com>
2019-06-28 01:18:03 -0400

    crawler-parse-finish

--- ArticleSpider/spiders/zhihu.py
+++ ArticleSpider/spiders/zhihu.py
@@ -1,6 +1,8 @@
 import re
 import scrapy
 import json
+from scrapy.loader import ItemLoader
+from items import ZhihuQuestionItem, ZhihuAnswerItem
 
 try:
     import urlparse as parse
@@ -11,6 +13,8 @@ class ZhihuSpider(scrapy.Spider):
     name = "zhihu"
     allowed_domains = ["www.zhihu.com"]
     start_urls = ['http://www.zhihu.com/']
+
+    start_answer_url = "https://www.zhihu.com/api/v4/questions/{0}/answers?sort_by=default&include=data%5B%2A%5D.is_normal%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccollapsed_counts%2Creviewing_comments_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Cmark_infos%2Ccreated_time%2Cupdated_time%2Crelationship.is_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cupvoted_followees%3Bdata%5B%2A%5D.author.is_blocking%2Cis_blocked%2Cis_followed%2Cvoteup_count%2Cmessage_thread_token%2Cbadge%5B%3F%28type%3Dbest_answerer%29%5D.topics&limit={1}&offset={2}"
 
     headers = {
         "HOST": "www.zhihu.com",
@@ -26,19 +30,81 @@ class ZhihuSpider(scrapy.Spider):
         for url in all_urls:
             match_obj = re.match("(.*zhihu.com/question/(\d+))(/|$).*", url)
             if match_obj:    
+                # if get question's page, then apply parse_question to extract information
                 request_url = match_obj.group(1)
                 question_id = match_obj.group(2)
-
-                yield scrapy.Request(request_url, headers=self.headers, callback=)
+                yield scrapy.Request(request_url, headers=self.headers, callback=self.parse_question)
+            else:
+                # if not get question's page then, explore further 
+                yield scrapy.Request(url, headers=self.headers, callback=self.parse)    
 
 
     def parse_question(self, response):
         # deal with question page, extract specific question item
         if "QuestionHeader-title" in response.text:  
             # deal with new version
+            match_obj = re.match("(.*zhihu.com/question/(\d+))(/|$).*", url)
+            if match_obj:    
+                question_id = int(match_obj.group(2))
+            item_loader = ItemLoader(item=ZhihuQuestionItem(), response=response)
+            item_loader.add_css("title", "h1.QuestionHeader-title::text")
+            item_loader.add_css("content", ".QuestionHeader-detail")
+            item_loader.add_value("url", response.url)
+            item_loader.add_value("zhihu_id", question_id)
+            item_loader.add_css("answer_num",".List-headerText span::text")
+            item_loader.add_css("comments_num",".QuestionHeader-actions button::text")
+            item_loader.add_css("watch_user_num", ".NumberBoard-value::text")
+            item_loader.add_css("topics", ".QuestionHeader-topics .Popover::text")
+
+            question_item = item_loader.load_item() 
         else    
             # deal with old version
+            match_obj = re.match("(.*zhihu.com/question/(\d+))(/|$).*", response.url)
+            if match_obj:
+                question_id = int(match_obj.group(2))
+
+            item_loader = ItemLoader(item=ZhihuQuestionItem(), response=response)
+            # item_loader.add_css("title", ".zh-question-title h2 a::text")
+            item_loader.add_xpath("title", "//*[@id='zh-question-title']/h2/a/text()|//*[@id='zh-question-title']/h2/span/text()")
+            item_loader.add_css("content", "#zh-question-detail")
+            item_loader.add_value("url", response.url)
+            item_loader.add_value("zhihu_id", question_id)
+            item_loader.add_css("answer_num", "#zh-question-answer-num::text")
+            item_loader.add_css("comments_num", "#zh-question-meta-wrap a[name='addcomment']::text")
+            # item_loader.add_css("watch_user_num", "#zh-question-side-header-wrap::text")
+            item_loader.add_xpath("watch_user_num", "//*[@id='zh-question-side-header-wrap']/text()|//*[@class='zh-question-followers-sidebar']/div/a/strong/text()")
+            item_loader.add_css("topics", ".zm-tag-editor-labels a::text")
+
+            question_item = item_loader.load_item()   
+
+        yield scrapy.Request(self.start_answer_url.format(question_id, 20, 0), headers=self.headers, callback=self.parse_answer)
+        yield question_item    
             
+    def parse_answer(self, response):
+
+        ans_json = json.loads(response.text)
+        is_end = ans_json["paging"]["is_end"]
+        totals_answer = ans_json["paging"]["totals"]
+        next_url = ans_json["paging"]["next"]
+
+        for answer in ans_json["data"]:
+            answer_item = ZhihuAnswerItem()
+            answer_item["zhihu_id"] = answer["id"]
+            answer_item["url"] = answer["url"]
+            answer_item["question_id"] = answer["question"]["id"]
+            answer_item["author_id"] = answer["author"]["id"] if "id" in answer["author"] else None
+            answer_item["content"] = answer["content"] if "content" in answer else None
+            answer_item["parise_num"] = answer["voteup_count"]
+            answer_item["comments_num"] = answer["comment_count"]
+            answer_item["create_time"] = answer["created_time"]
+            answer_item["update_time"] = answer["updated_time"]
+            answer_item["crawl_time"] = datetime.datetime.now()
+
+            yield answer_item
+
+        if not is_end:
+            yield scrapy.Request(next_url, headers=self.headers, callback=self.parse_answer)
+
 
     def start_requests(self):  
         return [scrapy.Request('https://www.zhihu.com/#signin', headers=self.headers, callback=self.login)]
